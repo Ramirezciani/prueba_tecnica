ec3de919ab1f85c3ffd248d355e501b505b79e04
Ramirezciani <pa.ramirezciani13@gmail.com>
2025-07-11 09:07:13 -0400

    initial config

---
+++ backend/app/database.py
@@ -0,0 +1,12 @@
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker
+import os
+
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql+asyncpg://user:password@db:5432/taskdb")
+
+engine = create_async_engine(DATABASE_URL, echo=True)
+AsyncSessionLocal = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+
+async def get_session():
+    async with AsyncSessionLocal() as session:
+        yield session
