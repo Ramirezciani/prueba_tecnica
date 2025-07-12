00d8d162d9b3cee01d42d7b2245790ff5d5ec7cf
Ramirezciani <pa.ramirezciani13@gmail.com>
2025-07-12 08:36:51 -0400

    fix some errors

--- backend/app/database.py
+++ backend/app/database.py
@@ -7,6 +7,7 @@ DATABASE_URL = os.getenv("DATABASE_URL", "postgresql+asyncpg://user:password@db:
 engine = create_async_engine(DATABASE_URL, echo=True)
 AsyncSessionLocal = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
-async def get_session():
+# Renombrada para que coincida con el import
+async def get_db():
     async with AsyncSessionLocal() as session:
         yield session
