39d0c5265e61114f5bfa0eeebd6cbc592669fe69
Ramirezciani <pa.ramirezciani13@gmail.com>
2025-07-14 14:15:26 -0400

    listo 2

--- frontend/mi-app/src/app/dashboard/dashboard.ts
+++ frontend/mi-app/src/app/dashboard/dashboard.ts
@@ -1,18 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common'; 
 import { Sidebar } from '../shared/sidebar/sidebar';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [Sidebar],
+  imports: [Sidebar, CommonModule],  
   templateUrl: './dashboard.html',
   styleUrls: ['./dashboard.css']
 })
 export class DashboardComponent implements OnInit {
   projects: any[] = [];
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
     this.fetchProjects();
